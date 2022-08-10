b651a95235999c7fa30a3ff2f76285b8f7d76ab0
Winnie Zhao <winniemaizhao@gmail.com>
2022-08-10 12:04:16 -0400

    seeded restaurants, created routes controller actions views

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,17 @@
+class RestaurantsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Seeding restaurants..."
+
+5.times do
+  restaurant = Restaurant.create!(name: Faker::Food.spice, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone, category: %w[chinese italian japanese french belgian].sample)
+  puts "Restaurant with id #{restaurant.id}"
+end
+
+puts "Done seeding"
