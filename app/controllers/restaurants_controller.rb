25636dcb94b0ff0a8d33ae7186a4815ee1f7d307
Winnie Zhao <winniemaizhao@gmail.com>
2022-08-10 13:41:51 -0400

    created form for new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,30 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show create]
+
   def index
 
   end
 
   def show
-
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
 
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
